780147134375e4e07615dc1a3c76e7ec5d5c9928
AbhiAbhiAbhii <abhilash@gradical.xyz>
2023-05-09 23:58:26 +0530

    tweaks

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,8 @@ import Hero from '../components/Slice/LandingPage/Hero/hero'
 import LogoShowcase from '../components/Slice/LandingPage/LogoShowcase/logoShowcase'
 import Purpose from '../components/Slice/LandingPage/Purpose/purpose'
 import ShowcaseA from '../components/Slice/LandingPage/ShowcaseA/showcaseA'
-import OurWork from '../components/Slice/LandingPage/ourWorks/ourWorks'
+// import OurWork from '../components/Slice/LandingPage/ourWorks/ourWorks'
+import OurWork from '../components/Slice/LandingPage/OurWorks/ourWorks'
 import Stats from '../components/Slice/LandingPage/Stats/stats'
 
 export default function Home() {
