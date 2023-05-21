08b5ff0491254905a8a2472d361e1694867d1215
AbhiAbhiAbhii <abhilash@gradical.xyz>
2023-05-21 17:56:27 +0530

    hamburger menu

--- components/header.js
+++ components/header.js
@@ -1,14 +1,57 @@
 /* eslint-disable @next/next/no-img-element */
 
 import Link from "next/link";
+import { useState } from "react";
 
 
 export default function Header(){
 
+    const [openBurger, setOpenBurger] = useState(false)
+
     let navItem = [{item: 'Home', link: '/'},{item: 'About us', link:'/about-us'},{item: 'Endeavours', link:'/endeavours'},{item: 'Awards and Gallery', link:'/awards&gallery'},{item: 'Contact', link:'/contact'}];
+
+     //anim
+     let quint = 'cubic-bezier(0.85, 0, 0.15, 1)';
+     let quart = 'cubic-bezier(0.76, 0.00, 0.24, 1.00)';
+
+    const hmbrgr = () => {
+        let nav = document.querySelector(".nav");
+        let lineA = document.querySelector('.line1')
+        let lineC = document.querySelector('.line3')
+
+
+        if (openBurger) {
+            nav.style.transform = 'translateY(0)';
+            lineA.style.transform = 'rotate(120deg)'
+            lineA.style.margin = '0'
+            lineA.style.width= '75%'
+            lineC.style.width= '75%'
+            lineC.style.transform = 'rotate(-120deg)'
+
+
+
+        }
+        else if(!openBurger) {
+            lineA.style.margin = '0 0 1em 0'
+
+            lineA.style.transform = 'rotate(0deg) '
+            lineA.style.width= '80%'
+            lineC.style.width= '80%'
+
+
+            lineC.style.transform = 'rotate(0deg)'
+
+            setTimeout(() => {
+                nav.style.transform = 'translateY(-200%)';
+            }, 500)
+
+        }
+        setOpenBurger(!openBurger)
+    }
     
     return(
-        <nav className="nav">
+        <>
+        <nav className="nav" style={{transition: `all 1.5s ${quint}`}}>
             <div className="nav_Container">
                 <Link href={'/'} style={{cursor:'pointer'}} className="logo_Container">
                     <img 
@@ -21,7 +64,7 @@ export default function Header(){
                         navItem.map((data, i) => {
 
                             return(
-                                <Link href={data.link} className="navItem" key={i}>
+                                <Link onClick={hmbrgr} href={data.link} className="navItem" key={i}>
                                     <p>{data.item}</p>
                                 </Link>
                             )
@@ -30,5 +73,21 @@ export default function Header(){
                 </div>
             </div>
         </nav>
+        {/* mobile */}
+        <nav className="mobNav" >
+            <div className="navMob_LogoContainer" style={{ height:'20em', width:'20em'}}>
+                <Link href={'/'} style={{cursor:'pointer'}}>
+                    <img 
+                        style={{height:'100%', width:'100%', objectFit:'cover'}} 
+                        src="/Landing/EIFCTLogo.svg" 
+                        alt="EIFCT"/>
+                </Link>
+            </div>
+            <div className="hamburger" onClick={hmbrgr} >
+                    <div className="line1" style={{transition: `all 1s ${quint}`}}></div>
+                    <div className="line3" style={{transition: `all 1s ${quint}`}}></div>
+            </div>
+        </nav>
+        </>
     )
 }
