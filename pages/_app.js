f6b301605351eb89d54117008a09387de0870ba2
AbhiAbhiAbhii <abhilash@gradical.xyz>
2023-06-10 15:08:01 +0530

    meta test

--- pages/_app.js
+++ pages/_app.js
@@ -27,32 +27,6 @@ import Head from '../components/head'
 export default function App({ Component, pageProps }) {
   return(
     <>
-    <Head>
-            {/* <!-- Primary Meta Tags --> */}
-            <title>EnhanceIndiaFoundation</title>
-            <meta name="title" content="EnhanceIndiaFoundation" />
-            <meta name="description" content="We believe that every person deserves the opportunity to live a life of dignity and purpose, and it is our goal to help make this a reality for as many people as possible." />
-
-            {/* <!-- Open Graph / Facebook --> */}
-            <meta property="og:type" content="website" />
-            <meta property="og:url" content="https://enhanceindiafoundation.com" />
-            <meta property="og:title" content="EnhanceIndiaFoundation" />
-            <meta property="og:description" content="We believe that every person deserves the opportunity to live a life of dignity and purpose, and it is our goal to help make this a reality for as many people as possible." />
-            <meta property="og:image" content="/meta-img.png" />
-
-            {/* <!-- Twitter --> */}
-            <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:url" content="https://enhanceindiafoundation.com" />
-            <meta property="twitter:title" content="EnhanceIndiaFoundation" />
-            <meta property="twitter:description" content="We believe that every person deserves the opportunity to live a life of dignity and purpose, and it is our goal to help make this a reality for as many people as possible." />
-            <meta property="twitter:image" content="/meta-img.png"></meta>
-
-            {/* favicon */}
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-            <link rel="manifest" href="/site.webmanifest"></link>
-        </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
